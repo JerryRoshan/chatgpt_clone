63b80e9a2dd06ad40efaa3870a5d93befda73d4e
JerryRoshan <112300973+JerryRoshan@users.noreply.github.com>
2023-06-21 16:35:15 +0530

    Project started : Next JS setup

---
+++ components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='flex items-center justify-center bg-black text-white w-screen h-screen overflow-hidden'>
+        Am i visible
+    </div>
+  )
+}
+
+export default Banner
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='flex items-center justify-center bg-red-600 text-white w-screen h-screen overflow-hidden'>
+        Am i visible
+    </div>
+  )
+}
+
+export default Banner
